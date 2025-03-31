df800ef4a11d0a57516378742058c6bb5c9ead00
Vivek Sharma <136906596+vs7174379@users.noreply.github.com>
2025-03-31 23:14:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -83,11 +83,9 @@ app.use((req,res,next)=>{
     next();
 })
 
-app.get("/listings",(req,res)=>{
-     res.send("Hi,i am root");
- })
 
-app.use("/listings",listingRouter)
+
+
 app.use("/listings/:id/reviews",reviewRouter)
 app.use("/",userRouter);
 
@@ -115,5 +113,7 @@ app.use((err,req,res,next)=>{
     let {statusCode=500,message="something went wrong!"}=err;
     res.status(statusCode).render("error.ejs",{err});
 })
+
+app.use("/listings",listingRouter)
 
 
