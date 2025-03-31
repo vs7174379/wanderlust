c0d71b7f63fbee89710a3b1f4cc0780996bdfa8e
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-03-31 22:38:31 +0530

    done

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ const sessionOptions={
         expires:Date.now()+7*24*60*60*1000,
         maxAge:7*24*60*60*1000,
         httpOnly:true
-    }
+    }                                                                                                                                                                                                                                                                                                                                                                                                                      
 }
 
 
--- models/Listing.js
+++ models/Listing.js
@@ -2,6 +2,8 @@ const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
 const Review=require("./reviews.js");
 const User=require("./user.js");
+
+
 const listingSchema=new Schema({
     title:{type:String,
         required:true,},
