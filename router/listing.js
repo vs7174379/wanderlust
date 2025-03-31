9e75036b5a5d7c1d6f0ed0169cefc28d852c64e6
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-03-31 22:59:51 +0530

    done

--- router/listing.js
+++ router/listing.js
@@ -9,7 +9,7 @@ const {isLoggedIn,isOwner,validateListing}=require("../middleware.js");
 
 const listingController=require("../controllers/listings.js");
 const multer  = require('multer')
-const {cloudinary,storage}=require("../cloudconfig.js");
+const {cloudinary,storage}=require("../cloudConfig.js");
 const upload = multer({ storage })
 
 
